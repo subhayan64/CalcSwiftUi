b7cdd9ec24624087520def03fc9be9e277864c04
subhayan.mukhopadhay <subhayan.mukhopadhyay@zestmoney.in>
2023-02-05 01:43:49 +0530

    Initial Commit

---
+++ CalcSwitUI/CalcSwitUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalcSwitUIApp.swift
+//  CalcSwitUI
+//
+//  Created by subhayan.mukhopadhay on 05/02/23.
+//
+
+import SwiftUI
+
+@main
+struct CalcSwitUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CalcSwitUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CalcSwitUI
+//
+//  Created by subhayan.mukhopadhay on 05/02/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
